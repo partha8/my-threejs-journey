c1c40ab6e73654f8637fc82327bb911dd9e47a5a
Partha Pratim Sarma <64675650+partha8@users.noreply.github.com>
2022-10-27 15:21:24 +0530

    graves added

--- 11-haunted-house/src/script.js
+++ 11-haunted-house/src/script.js
@@ -76,6 +76,27 @@ bush4.scale.set(0.15, 0.15, 0.15); // (x,y,z)
 bush4.position.set(-1, 0.05, 2.6);
 house.add(bush4);
 
+//graves
+const graves = new THREE.Group();
+scene.add(graves);
+
+const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2);
+const graveMaterial = new THREE.MeshStandardMaterial({ color: "#b2b6b1" });
+
+for (let i = 0; i < 50; i++) {
+  const angle = Math.random() * Math.PI * 2;
+  const radius = 3.2 + Math.random() * 6.5;
+  const x = Math.sin(angle) * radius;
+  const z = Math.cos(angle) * radius;
+
+  const grave = new THREE.Mesh(graveGeometry, graveMaterial);
+  grave.position.set(x, 0.3, z);
+  grave.rotation.y = (Math.random() - 0.5) * 0.4;
+  grave.rotation.z = (Math.random() - 0.5) * 0.4;
+
+  graves.add(grave);
+}
+
 // Floor
 const floor = new THREE.Mesh(
   new THREE.PlaneGeometry(20, 20),
